dd3a86a5f14a142f40faec8ad9285cbc1ff13f62
s0wnd <aimmvp@gmail.com>
2020-09-01 15:02:02 +0900

    application

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+// Test
     public static ApplicationContext applicationContext;
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(Application.class, args);
